9bc9202fda8a0820f08666bd67997664eeb61967
Will <willsimmons@gmail.com>
2015-07-15 14:27:59 -0700

    simple rtc sample added, now to build over it with our stuff

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-var express = require('express');
+// Load required modules
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var io      = require("socket.io");         // web socket external module
+var easyrtc = require("easyrtc");           // EasyRTC external module
+
+
+// Setup and configure Express http server. Expect a subfolder called "public" to be the web root.
 var app = express();
+app.use(express.static(__dirname + "/public/"));
+
+
+// Start Express http server on port 3000
+var webServer = http.createServer(app).listen(3000);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(app, socketServer);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Server Running at http://%s:%s Message of Sprint Day 2: Hold on to your butts -Jurassic Park', host, port);
+  res.send('index.html');
 });
